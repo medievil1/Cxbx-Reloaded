71ea5e7f75f1724eeee76ce0bd435b45dc928a86
PatrickvL <pvanlogchem@gmail.com>
2022-12-14 20:13:48 +0100

    Introduced Xbox version of X_D3DLOCKED_BOX (including its LOGRENDER)

--- src/core/hle/D3D8/Direct3D9/Direct3D9.h
+++ src/core/hle/D3D8/Direct3D9/Direct3D9.h
@@ -831,7 +831,7 @@ xbox::void_xt WINAPI EMUPATCH(Lock3DSurface)
 (
     X_D3DPixelContainer *pPixelContainer,
     uint_xt				Level,
-	D3DLOCKED_BOX		*pLockedVolume,
+	X_D3DLOCKED_BOX		*pLockedVolume,
 	D3DBOX				*pBox,
 	dword_xt				Flags
 );
@@ -913,7 +913,7 @@ xbox::void_xt WINAPI EMUPATCH(D3DVolumeTexture_LockBox)
 (
     X_D3DVolumeTexture *pThis,
     uint_xt                Level,
-    D3DLOCKED_BOX      *pLockedVolume,
+    X_D3DLOCKED_BOX      *pLockedVolume,
     CONST D3DBOX       *pBox,
     dword_xt               Flags
 );
